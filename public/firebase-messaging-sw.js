b15ccde1e8119fb737790b472a4b05f14540ee18
Amandeep <amanr5413@gmail.com>
2025-05-15 22:28:08 +0530

    [Amandeep] Add: update vercel.json

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,15 +1,7 @@
 importScripts('https://www.gstatic.com/firebasejs/10.14.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.14.1/firebase-messaging-compat.js');
 
-const firebaseConfig = {
-  // apiKey: "AIzaSyCKt2wYuYzr0uKWe8o5jUE6p9wb-3lSK68",
-  // authDomain: "movie-explorer-5bc8a.firebaseapp.com",
-  // projectId: "movie-explorer-5bc8a",
-  // storageBucket: "movie-explorer-5bc8a.firebasestorage.app",
-  // messagingSenderId: "561268525206",
-  // appId: "1:561268525206:web:9ba893c094bf72aed81ab7",
-  // measurementId: "G-XPP4G1SXPV"
-  
+const firebaseConfig = {  
    apiKey: "AIzaSyCHIX68-JJHkTEpPjFHoQCJO9jxD01_5XU",
   authDomain: "movieexplorer-57075.firebaseapp.com",
   projectId: "movieexplorer-57075",
