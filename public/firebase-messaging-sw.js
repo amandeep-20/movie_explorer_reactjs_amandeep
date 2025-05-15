e7e57f593ac657e364ba5ea9b58ba5de8d2fe3c8
Amandeep <amanr5413@gmail.com>
2025-05-15 22:35:48 +0530

    [Amandeep] Add: update messaging file

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -12,7 +12,6 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-// ✅ Correct way for compat version
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
@@ -25,31 +24,3 @@ messaging.onBackgroundMessage((payload) => {
 
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
-
-
-
-// importScripts('https://www.gstatic.com/firebasejs/10.14.1/firebase-app-compat.js');
-// importScripts('https://www.gstatic.com/firebasejs/10.14.1/firebase-messaging-compat.js');
-
-// const firebaseConfig = {  
-//    apiKey: "AIzaSyCHIX68-JJHkTEpPjFHoQCJO9jxD01_5XU",
-//   authDomain: "movieexplorer-57075.firebaseapp.com",
-//   projectId: "movieexplorer-57075",
-//   storageBucket: "movieexplorer-57075.firebasestorage.app",
-//   messagingSenderId: "972960795720",
-//   appId: "1:972960795720:web:a125cffdb37e3c420e1c7d"
-// };
-
-// const app = firebase.initializeApp(firebaseConfig);
-// const messaging = firebase.getMessaging(app);
-
-// firebase.onBackgroundMessage(messaging, (payload) => {
-//   console.log('[firebase-messaging-sw.js] Received background message:', payload);
-//   const notificationTitle = payload.notification.title;
-//   const notificationOptions = {
-//     body: payload.notification.body,
-//     icon: payload.notification.image || '/favicon.ico'
-//   };
-
-//   self.registration.showNotification(notificationTitle, notificationOptions);
-// });
